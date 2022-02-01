f72a5ca19ee44407cffb649b5a4fdac45777803f
Aaron <aaronnoraator@gmail.com>
2022-02-01 16:22:01 -0330

    added turtle

--- address.py
+++ address.py
@@ -1,3 +1,4 @@
+import turtle
 #Describes the idea/concept of a house
 class Address:
     def __init__(self,street, city, state, postal_code, country):
@@ -12,11 +13,15 @@ class Circle:
     def __init__(self, radius:float):
         self.radius = radius
     def getarea(self):
-        area = (3.14 * {self.radius}**2)
+        area = (3.14 * (self.radius)**2)
         return area
     def getcircum(self):
-        circ = (2*3.14*{self.radius})
+        circ = (2*3.14*(self.radius))
         return circ
+    def draw(self):
+        gary = turtle.Turtle()
+        gary.circle(self.radius)
+
 
 #Create address object
 my_house = Address(street="Purble Place",
@@ -36,3 +41,4 @@ bigcircle = Circle(radius=10)
 
 print(my_house.to_string())
 print(mediumcircle.getarea())
+bigcircle.draw()
